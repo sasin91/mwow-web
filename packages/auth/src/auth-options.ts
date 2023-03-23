598ad1c17f9c2de786b6a5ee7210717d4cd2442d
sasin91 <jonas.kerwin.hansen@gmail.com>
2023-03-23 14:11:12 +0100

    wip

--- packages/auth/src/auth-options.ts
+++ packages/auth/src/auth-options.ts
@@ -4,6 +4,7 @@ import DiscordProvider from "next-auth/providers/discord";
 import CredentialsProvider from "next-auth/providers/credentials";
 
 import { prisma } from "@acme/db";
+import bcrypt from 'bcryptjs';
 
 /**
  * Module augmentation for `next-auth` types
@@ -12,18 +13,18 @@ import { prisma } from "@acme/db";
  * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
  **/
 declare module "next-auth" {
-  interface Session extends DefaultSession {
-    user: {
-      id: string;
-      // ...other properties
-      // role: UserRole;
-    } & DefaultSession["user"];
-  }
+    interface Session extends DefaultSession {
+        user: {
+            id: string;
+            // ...other properties
+            // role: UserRole;
+        } & DefaultSession["user"];
+    }
 
-  // interface User {
-  //   // ...other properties
-  //   // role: UserRole;
-  // }
+    // interface User {
+    //   // ...other properties
+    //   // role: UserRole;
+    // }
 }
 
 /**
@@ -32,72 +33,59 @@ declare module "next-auth" {
  * @see https://next-auth.js.org/configuration/options
  **/
 export const authOptions: NextAuthOptions = {
-  callbacks: {
-    session({ session, user }) {
-      if (session.user) {
-        session.user.id = user.id;
-        // session.user.role = user.role; <-- put other properties on the session here
-      }
-      return session;
+    callbacks: {
+        session({ session, user }) {
+            if (session.user) {
+                session.user.id = user.id;
+                // session.user.role = user.role; <-- put other properties on the session here
+            }
+            return session;
+        },
     },
-  },
     adapter: PrismaAdapter(prisma),
-  providers: [
-    DiscordProvider({
-      clientId: process.env.DISCORD_CLIENT_ID as string,
-      clientSecret: process.env.DISCORD_CLIENT_SECRET as string,
-    }),
-    CredentialsProvider({
-      // The name to display on the sign in form (e.g. "Sign in with...")
-      name: "Credentials",
-      // `credentials` is used to generate a form on the sign in page.
-      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
-      // e.g. domain, username, password, 2FA token, etc.
-      // You can pass any HTML attribute to the <input> tag through the object.
-      credentials: {
-        username: { label: "Username", type: "text", placeholder: "jsmith" },
-        password: { label: "Password", type: "password" }
-      },
-      async authorize(credentials, req) {
-          console.log(credentials)
-          const account = await prisma.account.findFirst({
-              where: {
-                  username: credentials?.username
-              }
-          })
+    providers: [
+        DiscordProvider({
+            clientId: process.env.DISCORD_CLIENT_ID as string,
+            clientSecret: process.env.DISCORD_CLIENT_SECRET as string,
+        }),
+        CredentialsProvider({
+            // The name to display on the sign in form (e.g. "Sign in with...")
+            name: "Credentials",
+            // `credentials` is used to generate a form on the sign in page.
+            // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+            // e.g. domain, username, password, 2FA token, etc.
+            // You can pass any HTML attribute to the <input> tag through the object.
+            credentials: {
+                email: { label: "Email", type: "email" },
+                password: { label: "Password", type: "password" }
+            },
+            async authorize(credentials, req) {
+                const userWithPassword = await prisma.user.findUnique({
+                    where: { email: credentials!.email },
+                    include: {
+                        password: true,
+                    },
+                });
 
-          if (account) {
-              const salt = generateSalt();
-              const verifier = calculateSRP6Verifier(account.username, credentials?.password, salt);
-              if (verifier === account.verifier) {
-                // Any object returned will be saved in `user` property of the JWT
-                  const user: User = {
-                      id: verifier,
-                      name: account.username,
-                      email: account.email,
-                      image: 'https://static.wikia.nocookie.net/wowpedia/images/b/b3/Human_male.gif/revision/latest?cb=20090605181233'
+                if (!userWithPassword || !userWithPassword.password) {
+                    return null;
                   }
 
-                  return user
-              }
+                if (userWithPassword && bcrypt.compareSync(credentials!.password, userWithPassword.password.hash)) {
+                    return userWithPassword;
+                }
 
-              throw new Error("Invalid username or password")
-        } else {
-          // If you return null then an error will be displayed advising the user to check their details.
-          return null
-
-          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
-        }
-      }
-    })
-    /**
-     * ...add more providers here
-     *
-     * Most other providers require a bit more work than the Discord provider.
-     * For example, the GitHub provider requires you to add the
-     * `refresh_token_expires_in` field to the Account model. Refer to the
-     * NextAuth.js docs for the provider you want to use. Example:
-     * @see https://next-auth.js.org/providers/github
-     **/
-  ],
+                throw new Error("Invalid username or password")
+            }
+        })
+        /**
+         * ...add more providers here
+         *
+         * Most other providers require a bit more work than the Discord provider.
+         * For example, the GitHub provider requires you to add the
+         * `refresh_token_expires_in` field to the Account model. Refer to the
+         * NextAuth.js docs for the provider you want to use. Example:
+         * @see https://next-auth.js.org/providers/github
+         **/
+    ],
 };
--- packages/trinitycore/index.ts
+++ packages/trinitycore/index.ts
@@ -1,1 +1,9 @@
-// TODO
+import { createClientAsync } from 'soap'
+import type { Client } from 'soap'
+
+const globalForSoap = globalThis as unknown as { soap: Client };
+
+export const soap =
+    globalForSoap.soap || await createClientAsync(process.env.TRINITYCORE_SOAP_URL!, {
+        
+    });
